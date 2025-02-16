638c1512fe75dc7bbc97e63c90f10993ec9f6f32
anmekazuto <jaydenbledsoe1@gmail.com>
2025-02-15 22:10:20 -0600

    added features like working guess feature for quotes, picture popup when the guess is correct or you run out of lives, not able to guess again when you get it correct or run out of lives, started on the previous days feature, thinking of only having quotes and not images, might put a coming soon text nect to image button, in the future i want to add a profile button with stats, and make the site look better when the backend is done

--- quotes/quotes.js
+++ quotes/quotes.js
@@ -1,28 +1,100 @@
-let charinput = document.getElementById("charform").value;
+
+
+let imageContainer = document.getElementsByClassName("charimage");
+let image = document.getElementById("charimage");
+let Quote = document.getElementById("quote");
+let charinput = document.getElementById("charinput");
 let guesslives = document.getElementById("lives");
 let DailyQuote = [
-    {daily: 0 ,quote:"Anyone who tries to hurt my friends...is gonna pay!",
-        characterimg:"/resources/goku-dragon-ball" ,
-        answer:"Goku"}
+    {
+        daily: 0 ,
+        quote:"Anyone who tries to hurt my friends...is gonna pay!",
+        characterimg:"/resources/goku-dragon-ball.jpg" ,
+        answer:"goku"
+    },
+    {
+        daily:1,
+        quote:"The first game, second game, the playoffs, the nationals... I'm going to win them all.",
+        characterimg:"/resources/kageyama-kaikyuu.pdf",
+        answer: "kageyama"     
+    },
+
 ]
+/* makes everything work*/
 let day = 0;
 let lives = 3;
 guesslives.innerHTML = lives;
 let answer = DailyQuote[day].answer;
+let dailyQ = DailyQuote[day].quote;
+let dailyI = DailyQuote[day].characterimg;    
+
+/* functions for when the answer is right */
+function correct(){
+
+    let RightAnswer = "That answer is correct!Try previous days!"
+    guesslives.innerHTML = RightAnswer;
+    console.log(charinput.value);
+    console.log(lives);
+}
+/* functions for showing the image */
+
+function ShowImg(){
+    
+    image.src = DailyQuote[day].characterimg;
+    image.style.display = "inline-block";
+    
+}
+
+
 
 function Charguess(){
+//char input doesnt work not getting value from input
+    if(charinput.value == answer){
+        
+        correct();
+        ShowImg();
+    }
     
-    if(charinput == answer){
-        console.log(charinput);
-        console.log(lives);
-    }
-    if(charinput != answer){
+    if(charinput.value != answer){
         lives -= 1;
+        guesslives.innerHTML = lives;
         console.log(lives);
+        console.log(charinput.value)
+
     }
-    guesslives.innerHTML = lives;
-    // code below not working 
-    if(lives = 0 ){
-        guesslives.innerHTMl = `Game Over! The Answer Is ${answer}`
+    
+    
+    
+    if(lives <= 0){
+        
+        let newlives = `Game Over! The Answer Is ${answer}`
+        guesslives.innerHTML = newlives;
+        ShowImg();
+        
     }
+    
 }
+
+// function for setting the daily quote image and answer
+function Daily(){
+    //quote
+    Quote.innerHTML = dailyQ; 
+    //image (image not showing)
+    image.src = dailyI;
+
+
+    
+
+
+}
+
+Daily();
+
+function PreviousDays(){
+    for ( i in DailyQuote){
+        console.log(i);
+        document.createElement("button")
+    }
+
+}
+PreviousDays()
